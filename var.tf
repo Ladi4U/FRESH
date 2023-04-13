243b0474ae32d13e4ef46595af2ff0071e794b78
Ladi4U <128870718+Ladi4U@users.noreply.github.com>
2023-04-12 21:04:50 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,16 +1,16 @@
 variable "project_id" {
   description = "The ID of the GCP project."
-  default     = "avian-amulet-378416"
+  default     = "robust-form-382822"
 }
 
 variable "bucket_name" {
   description = "A list of unique names for the GCP storage buckets."
   type        = list(string)
   default     = [
-    "my-test-bucket-latest8999",
-    "mytestbucket8888",
-    "montrealcollege-927218-for-demo",
-    "bucket-full-of-happiness"
+    "my-beest-bucket-testclasses",
+    "mytestbucket9m",
+    "montrealcollege-9008-for-demo",
+    "bucket-full-of-Joy&happiness"
 
   ]
 }
